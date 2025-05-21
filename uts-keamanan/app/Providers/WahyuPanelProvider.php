f9cf719f721987805baedfbd8442853d67fed8fd
Lex <wahyupamungkasjk@gmail.com>
2025-05-21 07:57:20 +0700

    Update WahyuPanelProvider.php

--- uts-keamanan/app/Providers/WahyuPanelProvider.php
+++ uts-keamanan/app/Providers/WahyuPanelProvider.php
@@ -18,7 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-class RajihPanelProvider extends PanelProvider
+class WahyuPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
